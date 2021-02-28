0b95864790e2bc3d6f853e446ff440e80a989888
ndkkahm <ndkkahm@gmail.com>
2021-02-28 21:57:11 +0900

    add doti.ch04.WordCount.java

---
+++ src/main/java/doit/ch04/WordCount.java
@@ -0,0 +1,68 @@
+package doit.ch04;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class WordCount {
+    public static class MyMappter extends Mapper<LongWritable, Text, Text, LongWritable> {
+        private final static LongWritable one = new LongWritable(1);
+        private Text word = new Text();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line, " \t\r\n\f`~!@#$%^&*()_-=+[{]}\\|;:'\",<.>/?");
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken().toLowerCase());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+        private LongWritable sumWritable = new LongWritable();
+
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            long sum = 0;
+            for(LongWritable value : values) {
+                sum += value.get();
+            }
+            sumWritable.set(sum);
+            context.write(key, sumWritable);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "WordCount");
+
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(MyMappter.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputValueClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
